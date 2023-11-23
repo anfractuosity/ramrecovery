d16f9130f686d6cad69a4186c4b0817a2f4250f4
anfractuosity <anfractuosity@anfractuosity.com>
2023-11-23 19:15:29 +0000

    Restructure code, so we fill as much memory as possible with image

--- src/load.c
+++ src/load.c
@@ -3,28 +3,59 @@
 #include <string.h>
 #include <unistd.h>
 
-#define IMAGES 5100U
-#define SIZE 725444U
+// https://stackoverflow.com/questions/2513505/how-to-get-available-memory-c-g
+unsigned long long getTotalSystemMemory()
+{
+    long pages = sysconf(_SC_PHYS_PAGES);
+    long page_size = sysconf(_SC_PAGE_SIZE);
+    return pages * page_size;
+}
 
 int main()
 {
-	char *buffer = malloc(SIZE);
+	// Load image
 	FILE *fp = fopen("mona.tga", "rb");
-	fread(buffer, SIZE, 1, fp);
 
-	char *buffer2 = malloc(SIZE * IMAGES);
-	int i = 0;
-	for (i = 0; i < IMAGES; i++) {
-		printf("%u\n", (i * SIZE));
-		memcpy(buffer2 + (i * SIZE), buffer, SIZE);
+	// Get size of Mona file
+	fseek(fp, 0L, SEEK_END);
+	int monasize = ftell(fp);
+	rewind(fp);
+
+	// Read image to buffer
+	char *buffer = malloc(monasize);
+	fread(buffer, monasize, 1, fp);
+
+	// Try to guess how much memory is free and make sure to subtract
+	// an amount to avoid getting OOM killed (200MB in this case).
+	unsigned long size = getTotalSystemMemory() - (1024 * 1024 * 200);
+	char *buffer2 = NULL;
+
+	// Try amounts decreasing by 50MB at a time, till we are able to allocate
+	// memory (or stop attempting at 512MB)
+	for (; size >= 1024 * 1024 * 512; size -= 1024 * 1024 * 50) {
+		printf("Attempting to allocate %lu bytes\n", size);
+		buffer2 = malloc(size);
+		if (buffer2 != NULL)
+			break;
 	}
 
-	printf("Done\n");
-	fflush(stdout);
+	if (buffer2) {
+		// Fill memory with Mona Lisa
+		long i = 0;
+		while ((i * monasize) + monasize < size) {
+			printf("%lu\n", (i * monasize));
+			memcpy(buffer2 + (i * monasize), buffer, monasize);
+			i++;
+		}
 
-	while (1) {
-		sleep(5);
-	}
+		printf("Done\n");
+		fflush(stdout);
+
+		while (1) {
+			sleep(5);
+		}
+	} else
+		fprintf(stderr, "Failed\n");
 
 	fclose(fp);
 }
